f8e16f25234654c0a0ab3ed8937554a5cf2127af
RioCantre <78406788+RioCantre@users.noreply.github.com>
2021-02-17 15:31:08 +0100

    Updated html ,css and js
    Eliminated unnecessary details on html and updated the css, cleared the Js file and started making new function to show the current temperature of a specific city.

--- src/index.js
+++ src/index.js
@@ -1,67 +1,19 @@
-let now = new Date();
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-let weekday = weekdays[now.getDay()];
-let nowToday = document.querySelector(".day-name");
-nowToday.innerHTML = `${weekday}`;
-let time = document.querySelector(".today-datetime");
-time.innerHTML = `${hours}:${minutes}`;
 
-function locationForm(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input");
-  let currentCity = document.querySelector("#Main-city");
-  citySearch(city.value);
-  currentCity.innerHTML = city.value;
-}
-let currentCity = document.querySelector("form");
-currentCity.addEventListener("submit", locationForm);
-
-// week 5 homework
-function citySearch(currentCity) {
-  let apiKey = "3312911a0b7ca102a3fa47c9257e12fa";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(weatherToday);
-}
-// citySearch("New York");
-
-function weatherToday(response) {
-  let currentCity = response.data.name;
-  let searchCity = document.querySelector("#Main-city");
-  searchCity.innerHTML = currentCity;
-  
-
-  let description = response.data.weather[0].main;
-  let searchCityDesc = document.querySelector(".description");
-  searchCityDesc.innerHTML = description;
-
-  let temperature = Math.round(response.data.main.temp);
-  let currentCityTemp = document.querySelector(".today-temp");
-  currentCityTemp.innerHTML = temperature;
+function showCurrenttemperature(response) {
+  console.log(response.data);
+  let currentCity = document.querySelector("#main-city");
+  let currentDescription = document.querySelector(".description");
+  let currentTemperature = document.querySelector(".today-temp");
+  let currentHumidity = document.querySelector(".current-humid");
+  let currentWindspeed = document.querySelector(".current-wind");
+  currentCity.innerHTML = response.data.name;
+  currentDescription.innerHTML = response.data.weather[0].description;
+  currentTemperature.innerHTML = Math.round(response.data.main.temp);
+  currentHumidity.innerHTML = response.data.main.humidity;
+  currentWindspeed.innerHTML = Math.round(response.data.wind.speed);
 }
 
-function posNow(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(currentPosition);
-}
+let apiKey = "3312911a0b7ca102a3fa47c9257e12fa";
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
-function currentPosition(position) {
-  let apiKey = "3312911a0b7ca102a3fa47c9257e12fa";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  axios.get(url).then(weatherToday);
-}
-
-let requestPosition = document.querySelector("#current-input");
-requestPosition.addEventListener("click", posNow);
+axios.get(apiUrl).then(showCurrenttemperature);
