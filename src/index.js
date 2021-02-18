f4d8ffa95354d14f31b21ea3093e546fabee010f
RioCantre <78406788+RioCantre@users.noreply.github.com>
2021-02-18 15:17:39 +0100

    Added daily forecast

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ function currentTimeformat(timestamp) {
 
 
 function showCurrenttemperature(response) {
-  console.log(response.data);
+  // console.log(response.data);
   let currentCity = document.querySelector("#main-city");
   let currentDescription = document.querySelector(".description");
   let currentTemperatureinCel = document.querySelector(".cel-temp");
@@ -74,13 +74,34 @@ function showCurrenttemperature(response) {
   let currentHumidity = document.querySelector(".current-humid");
   let currentWindspeed = document.querySelector(".current-wind");
   let currentDateTime = document.querySelector(".current-time");
+  let timestamp = response.data.dt;
+  let timezone = response.data.timezone;
+  let showtimeStamp = timestamp + timezone;
+  let currenttimeStamp = showtimeStamp * 1000;
+  // console.log(timestamp);
+  // console.log(new Date());
+
+
+
   currentCity.innerHTML = response.data.name;
   currentDescription.innerHTML = response.data.weather[0].description;
   currentTemperatureinCel.innerHTML = Math.round(response.data.main.temp);
   currentTemperatureinFah.innerHTML = Math.round((response.data.main.temp *9)/5 + 32);
   currentHumidity.innerHTML = response.data.main.humidity;
   currentWindspeed.innerHTML = Math.round(response.data.wind.speed);
-  currentDateTime.innerHTML = currentTimeformat(response.data.dt * 1000);
+  currentDateTime.innerHTML = currentTimeformat(currenttimeStamp);
+
+
+  let apiKey = "3312911a0b7ca102a3fa47c9257e12fa";
+  let lat = response.data.coord.lat;
+  let lon = response.data.coord.lon;
+  let exclude = "current,alert";
+  let apiforecastdailyUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&appid=${apiKey}&units=metric`;
+  axios.get(apiforecastdailyUrl).then(showdailyForecast);
+
+  
+
+
 
   let currentMainIcon = document.querySelector(".main-description");
   let iconWeather = response.data.weather[0].icon;
@@ -89,10 +110,10 @@ function showCurrenttemperature(response) {
     currentMainIcon.setAttribute("src", `image/01cs.png`);
     currentMainIcon.setAttribute("alt",  response.data.weather[0].icon);   
   } else  if (iconWeather === "01d") {
-    document.querySelector(".weather-quote").innerHTML = `"Whenever you go, no matter the weather, always bring your own sunshine."`;
+    document.querySelector(".weather-quote").innerHTML = `"Wherever you go, no matter the weather, always bring your own sunshine."`;
     currentMainIcon.setAttribute("src", `image/02cs.png`);
     currentMainIcon.setAttribute("alt",  response.data.weather[0].icon);   
-  } else  if (iconWeather === "2d0" || iconWeather === "02n") {
+  } else  if (iconWeather === "02d" || iconWeather === "02n") {
     document.querySelector(".weather-quote").innerHTML = ` "Clouds are the sky's imagination."`;
     currentMainIcon.setAttribute("src", `image/03c.png`);
     currentMainIcon.setAttribute("alt",  response.data.weather[0].icon);   
@@ -126,12 +147,71 @@ function showCurrenttemperature(response) {
     currentMainIcon.setAttribute("alt",  response.data.weather[0].icon);   
   }
 
+}         
+
+function forecastDays(timestamp) {
+  let forecastDate = new Date(timestamp * 1000);
+  let day = forecastDate.getDay();
+  let forecastDaynames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  return forecastDaynames[day];
 }
+
+
+
+function showdailyForecast(response) {
+  let currentForecast = document.querySelector("#days");
+  let forecastDaily = null;
+  currentForecast.innerHTML = null;
+  // console.log(response.data);
+  
+  for (index = 1; index < 7; index ++) {
+    forecastDaily = response.data.daily[index];
+    let forecast = forecastDays(forecastDaily.dt);
+    let dailyIcon = forecastDaily.weather[0].icon;
+    if (dailyIcon === "01n") {
+      forecastDailySrc = `image/01cs.png`;
+    } else if (dailyIcon === "01d"){
+      forecastDailySrc = `image/02cs.png`;
+    } else if (dailyIcon === "02d" || dailyIcon === "02n"){
+      forecastDailySrc = `image/03c.png`;
+    } else if (dailyIcon === "03d" || dailyIcon === "03n"){
+      forecastDailySrc = `image/04bc.png`;
+    } else if (dailyIcon === "04d" || dailyIcon === "04n"){
+      forecastDailySrc = `image/04bc.png`;
+    } else if (dailyIcon === "09d" || dailyIcon === "09n"){
+      forecastDailySrc = `image/07sr.png`;
+    } else if (dailyIcon === "10d" || dailyIcon === "10n"){
+      forecastDailySrc = `image/06r.png`;
+    } else if (dailyIcon === "11d" || dailyIcon === "11n"){
+      forecastDailySrc = `image/08t.png`;
+    } else if (dailyIcon === "13d" || dailyIcon === "13n"){
+      forecastDailySrc = `image/05sn.png`;
+    } else if (dailyIcon === "50d" || dailyIcon === "50n"){
+      forecastDailySrc = `image/09f.png`;
+    }
+
+
+    currentForecast.innerHTML += `
+    <section>
+      <h3 class="weekname"> ${forecast}</h3>
+      <img class="outcome" src="${forecastDailySrc}" />
+        &nbsp;&nbsp;&nbsp;&nbsp;
+      <span class="degree">
+        <span class="degree-cel"> ${Math.round(forecastDaily.temp.min)}</span>° | <span class="degree-fah">${Math.round(forecastDaily.temp.max)}</span>°
+      </span>
+    </section>`
+
+
+  }
+}
+
 function searchCity(currentTypecity){
   let apiKey = "3312911a0b7ca102a3fa47c9257e12fa";
-  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${currentTypecity}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showCurrenttemperature);
-
+  let apicurrentUrl = `http://api.openweathermap.org/data/2.5/weather?q=${currentTypecity}&appid=${apiKey}&units=metric`;
+  axios.get(apicurrentUrl).then(showCurrenttemperature);
+  
+  let apiforecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${currentTypecity}&appid=${apiKey}&units=metric`;
+  axios.get(apiforecastUrl).then(showdailyForecast);
 }
 
 function currentCitysearch(event){
@@ -159,4 +239,4 @@ function currentLocation(position){
 let currentPosition = document.querySelector("#current-input");
 currentPosition.addEventListener("click", clickPositionlocation);
 
-searchCity("Los Angeles");
+searchCity("New York");
